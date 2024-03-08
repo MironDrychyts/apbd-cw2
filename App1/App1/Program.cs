4b575cffcce3a21f7b096804941a1b0da4fe90b3
s24113 <s24113@pjwstk.edu.pl>
2024-03-08 13:37:48 +0100

    Add GetAverage method

--- App1/App1/Program.cs
+++ App1/App1/Program.cs
@@ -6,3 +6,18 @@ Console.WriteLine("New change2");
 
 
 
+static double GetAverage(int[] numbers)
+{
+    double sum = 0;
+    
+    for (int i = 0; i < numbers[i]; i++)
+    {
+        sum += numbers[i];
+    }
+
+    return sum / numbers.Length;
+}
+
+//int[] numbs = new[] {1,2,3,4};
+
+//double avg = GetAverage(numbs);
