37da524549d131eb4555ee929dc822cd2fe1e7d7
s24113 <s24113@pjwstk.edu.pl>
2024-03-08 13:16:30 +0100

    Modyfikacja 1

--- App1/App1/Program.cs
+++ App1/App1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("New change");
-Console.WriteLine("New change2");
+Console.WriteLine("New change1");
+
+
